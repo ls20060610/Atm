e488cf97ef9985020a233734ffaaa0c444eb80a7
ls20060610 <a0930325385@gmail.com>
2017-11-28 11:00:23 +0800

    change

--- app/src/main/java/com/tom/atm/MainActivity.java
+++ app/src/main/java/com/tom/atm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tom.atm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -20,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        if(!logon){
+            Intent intent = new Intent(this,LoginActivity.class );
+            startActivity(intent);
+
+        }
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
